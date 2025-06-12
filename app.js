02396a807c860c58b0e73fdc957fd7906f2d21f1
EllmanT <tmuranda1@gmail.com>
2025-06-12 13:45:54 +0200

    added cors

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ import express from "express";
 
 import { PORT } from "./config/env.js";
 
+import cors from "cors";
 import errorMiddleware from "./middlewares/error.middleware.js";
 import cookieParser from "cookie-parser";
 import docProcessingRouter from "./routes/docProcessing.route.js";
 
 const app = express();
 
-
+app.use(cors({
+    origin: 'http://localhost:4200'|"http:localhost:3000"|"https://app.fiscalcloud.co.zw ", // or use a list of domains
+    credentials: true,
+}));
 // Built in node js middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
